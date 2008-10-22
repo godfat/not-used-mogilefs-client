877338e485c614fc7539a47cab7ae2bf8597382d
Eric Wong <normalperson@yhbt.net>
2008-10-21 18:55:29 -0700

    MySQL: switch to async interface
    This lets us use multiple green threads safely, but means
    we now require the async mysqlplus driver available here:
    
      http://github.com/oldmoe/mysqlplus/tree/master

--- lib/mogilefs/mysql.rb
+++ lib/mogilefs/mysql.rb
@@ -9,6 +9,7 @@ require 'mysql'
 class MogileFS::Mysql
 
   attr_accessor :domain
+  attr_reader :my
 
   def initialize(param = {})
     @domain = param[:domain]
@@ -36,39 +37,37 @@ class MogileFS::Mysql
     end
 
     raise MogileFS::Backend::InvalidCharsError if /[%\\]/ =~ prefix
-    prefix.gsub!(/_/, '\_')
-    prefix << '%'
+    prefix.gsub!(/_/, '\_') # not sure why MogileFS::Worker::Query does this...
 
-    st = @my.prepare <<-EOS
-      SELECT dkey,length,devcount FROM file
-      WHERE dmid = ? AND dkey LIKE ? AND dkey > ?
-      ORDER BY dkey LIMIT #{limit}
+    sql = <<-EOS
+    SELECT dkey,length,devcount FROM file
+    WHERE dmid = #{dmid}
+      AND dkey LIKE '#{@my.quote(prefix)}%'
+      AND dkey > '#{@my.quote(after)}'
+    ORDER BY dkey LIMIT #{limit}
     EOS
-    st.execute(dmid, prefix, after)
 
     keys = []
-    st.each do |dkey,length,devcount|
+    @my.c_async_query(sql).each do |dkey,length,devcount|
       yield(dkey, length, devcount) if block_given?
       keys << dkey
     end
     return [ keys, keys.last || '']
-    ensure
-      st.close rescue nil
   end
 
   def size(key)
     dmid = refresh_domain[@domain] or \
       raise MogileFS::Backend::DomainNotFoundError
-    st = @my.prepare('SELECT length FROM file ' \
-                     'WHERE dmid = ? AND dkey = ? LIMIT 1')
-    st.execute(dmid, key)
-    unless res = st.fetch
-      raise MogileFS::Backend::UnknownKeyError
-      return
-    end
-    return res[0]
-    ensure
-      st.close rescue nil
+
+    sql = <<-EOS
+    SELECT length FROM file
+    WHERE dmid = #{dmid} AND dkey = '#{@my.quote(key)}'
+    LIMIT 1
+    EOS
+
+    res = @my.c_async_query(sql).fetch_row
+    return res[0] if res && res[0]
+    raise MogileFS::Backend::UnknownKeyError
   end
 
   def get_paths(key, noverify = true, zone = nil)
@@ -76,30 +75,26 @@ class MogileFS::Mysql
       raise MogileFS::Backend::DomainNotFoundError
     devices = refresh_device or raise MogileFS::Backend::NoDevicesError
     urls = []
-    st = @my.prepare('SELECT fid FROM file WHERE dmid = ? AND dkey = ? LIMIT 1')
-    st.execute(dmid, key)
-    unless res = st.fetch
-      raise MogileFS::Backend::UnknownKeyError
-      return
+    sql = <<-EOS
+    SELECT fid FROM file
+    WHERE dmid = #{dmid} AND dkey = '#{@my.quote(key)}'
+    LIMIT 1
+    EOS
+
+    res = @my.c_async_query(sql).fetch_row
+    res && res[0] or raise MogileFS::Backend::UnknownKeyError
+    fid = res[0]
+    sql = "SELECT devid FROM file_on WHERE fid = '#{@my.quote(fid)}'"
+    @my.c_async_query(sql).each do |devid,|
+      devinfo = devices[devid.to_i]
+      port = devinfo[:http_get_port] || devinfo[:http_port] || 80
+      host = zone && zone == 'alt' ? devinfo[:altip] : devinfo[:hostip]
+      nfid = '%010u' % fid
+      b, mmm, ttt = /(\d)(\d{3})(\d{3})(?:\d{3})/.match(nfid)[1..3]
+      uri = "/dev#{devid}/#{b}/#{mmm}/#{ttt}/#{nfid}.fid"
+      urls << "http://#{host}:#{port}#{uri}"
     end
-    if fid = res[0]
-      st.prepare('SELECT devid FROM file_on WHERE fid = ?')
-      st.execute(fid)
-      st.each do |devid,|
-        devinfo = devices[devid.to_i]
-        port = devinfo[:http_get_port] || devinfo[:http_port] || 80
-        host = zone && zone == 'alt' ? devinfo[:altip] : devinfo[:hostip]
-        nfid = '%010u' % fid
-        b, mmm, ttt = /(\d)(\d{3})(\d{3})(?:\d{3})/.match(nfid)[1..3]
-        uri = "/dev#{devid}/#{b}/#{mmm}/#{ttt}/#{nfid}.fid"
-        urls << "http://#{host}:#{port}#{uri}"
-      end
-    else
-      urls = nil
-    end
-    return urls
-    ensure
-      st.close rescue nil
+    urls
   end
 
   private
@@ -117,15 +112,14 @@ class MogileFS::Mysql
     def refresh_device(force = false)
       return @cache_device if ! force && ((Time.now - @last_update_device) < 60)
       tmp = {}
-      @my.query(GET_DEVICES) do |res|
-        res.each do |devid, hostip, altip, http_port, http_get_port|
-          tmp[devid.to_i] = {
-            :hostip => hostip.freeze,
-            :altip => altip.freeze,
-            :http_port => http_port ? http_port.to_i : nil,
-            :http_get_port => http_get_port ? http_get_port.to_i : nil,
-          }.freeze
-        end
+      res = @my.c_async_query(GET_DEVICES)
+      res.each do |devid, hostip, altip, http_port, http_get_port|
+        tmp[devid.to_i] = {
+          :hostip => hostip.freeze,
+          :altip => altip.freeze,
+          :http_port => http_port ? http_port.to_i : nil,
+          :http_get_port => http_get_port ? http_get_port.to_i : nil,
+        }.freeze
       end
       @last_update_device = Time.now
       @cache_device = tmp.freeze
@@ -134,9 +128,8 @@ class MogileFS::Mysql
     def refresh_domain(force = false)
       return @cache_domain if ! force && ((Time.now - @last_update_domain) < 5)
       tmp = {}
-      @my.query('SELECT dmid,namespace FROM domain') do |res|
-        res.each { |dmid,namespace| tmp[namespace] = dmid.to_i }
-      end
+      res = @my.c_async_query('SELECT dmid,namespace FROM domain')
+      res.each { |dmid,namespace| tmp[namespace] = dmid.to_i }
       @last_update_domain = Time.now
       @cache_domain = tmp.freeze
     end
